87cf731f0b0b129c3b322d6db0e0426ef55ed01d
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2025-03-16 13:51:50 -0400

    Start working on FAT formatter

---
+++ BadBuilder.Formatter/FAT32BootSector.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BadBuilder.Formatter
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    internal struct FAT32BootSector
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public byte[] JumpCode;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public byte[] OEMName;
+
+        public ushort BytesPerSector;
+        public byte SectorsPerCluster;
+        public ushort ReservedSectorCount;
+        public byte NumberOfFATs;
+        public ushort MaxRootEntries;  // Unused in FAT32
+        public ushort TotalSectors16;  // If 0, use TotalSectors
+        public byte MediaDescriptor;
+        public ushort SectorsPerFAT16; // Unused in FAT32
+        public ushort SectorsPerTrack;
+        public ushort NumberOfHeads;
+        public uint HiddenSectors;
+        public uint TotalSectors;      // Total sectors (if TotalSectors16 is 0)
+
+        // FAT32-specific fields
+        public uint SectorsPerFAT;
+        public ushort FATFlags;
+        public ushort FileSystemVersion;
+        public uint RootCluster;
+        public ushort FSInfoSector;
+        public ushort BackupBootSector;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+        public byte[] Reserved;
+
+        public byte DriveNumber;
+        public byte Reserved1;
+        public byte BootSignature;
+        public uint VolumeID;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 11)]
+        public byte[] VolumeLabel;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public byte[] FileSystemType;
+    }
+}
---
+++ BadBuilder.Formatter/FAT32Utilities.cs
@@ -0,0 +1,28 @@
+﻿namespace BadBuilder.Formatter
+{
+    static class FAT32Utilities
+    {
+        internal static uint GetVolumeID()
+        {
+            DateTime now = DateTime.Now;
+
+            ushort low = (ushort)(now.Day + (now.Month << 8));
+            low += (ushort)((now.Millisecond / 10) + (now.Second << 8));
+
+            ushort hi = (ushort)(now.Minute + (now.Hour << 8));
+            hi += (ushort)now.Year;
+
+            return (uint)(low | (hi << 16));
+        }
+
+        internal static uint CalculateFATSize(uint diskSize, uint reservedSectorCount, uint sectorsPerCluster, uint numberOfFATs, uint bytesPerSector)
+        {
+            const ulong fatElementSize = 4;
+
+            ulong numerator = fatElementSize * (diskSize - reservedSectorCount);
+            ulong denominator = (sectorsPerCluster * bytesPerSector) + (fatElementSize * numberOfFATs);
+
+            return (uint)((numerator / denominator) + 1);
+        }
+    }
+}
